117e3c943ef280b133bf23743a51a938413e871b
boba166Ghub <boba166@yahoo.com>
2023-07-09 10:41:37 -0400

    the feature planet1 is added and working fine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
         solarSystemBasic Omnax = new solarSystemBasic();
-        Omnax.Moon="2";
-        Omnax.Sun="1";
-        Omnax.Planets="2";
-        Omnax.Star="4";
+        Omnax.Moon = "2";
+        Omnax.Sun = "1";
+        Omnax.Planets = "2";
+        Omnax.Star = "4";
 
-        FeatureSun AdamX=new FeatureSun();
-        AdamX.name="poya";
-        AdamX.temp="12344231 celcius";
-        AdamX.size="32942Km";
+        FeatureSun AdamX = new FeatureSun();
+        AdamX.name = "poya";
+        AdamX.temp = "12344231 celcius";
+        AdamX.size = "32942Km";
 
 
+        Planet1 xeon = new Planet1();
+        xeon.name = "reon";
+        xeon.color = "pink";
+        xeon.temp = "-45celcius";
+        xeon.size = "1cm";
 
     }
 
